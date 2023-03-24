e54b63edc1da260aa6ec756b3223abbc7166349d
Linus Heinsvig <heinsvig@icloud.com>
2023-03-24 18:23:29 +0000

    Add aria-labels for links

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,6 @@ let result = document.getElementById('result-box')
 let rock = document.getElementById('rock');
 let paper = document.getElementById('paper')
 let scissors = document.getElementById('scissors')
-let yourScore = document.getElementById('your-score')
-let oponentScoreDiv = document.getElementById('oponent-score')
 
 // Generates computers choice
 function getComputerChoice() {
@@ -28,23 +26,29 @@ function win(userChoice, computerChoice) {
     userScore++;
     userScoreSpan.innerHTML = userScore;
     result.innerHTML = `${convertToI(userChoice)}  Beats  ${convertToI(computerChoice)}`;
-    result.classList.add('correct')
-    setTimeout(function() { result.classList.remove('correct') }, 1000);
+    result.classList.add('correct');
+    setTimeout(function () {
+        result.classList.remove('correct')
+    }, 1000);
 }
 
 function lose(userChoice, computerChoice) {
     oponentScore++;
     oponentScoreSpan.innerHTML = oponentScore;
     result.innerHTML = `${convertToI(userChoice)}  Gets Beaten By  ${convertToI(computerChoice)}`;
-    result.classList.add('wrong')
-    setTimeout(function() { result.classList.remove('wrong') }, 1000);
+    result.classList.add('wrong');
+    setTimeout(function () {
+        result.classList.remove('wrong')
+    }, 1000);
 }
 
 function draw(userChoice, computerChoice) {
     result.innerHTML = "It's a Draw!";
     result.innerHTML = `${convertToI(userChoice)}  Cant't Do Anything To  ${convertToI(computerChoice)}`;
     result.classList.add('draw')
-    setTimeout(function() { result.classList.remove('draw') }, 1000);
+    setTimeout(function () {
+        result.classList.remove('draw')
+    }, 1000);
 }
 
 // Main Game function
@@ -73,15 +77,15 @@ function game(userChoice) {
 function main() {
     rock.addEventListener('click', function () {
         game('rock');
-    })
+    });
 
     paper.addEventListener('click', function () {
         game('paper');
-    })
+    });
 
     scissors.addEventListener('click', function () {
-        game('scissors')
-    })
+        game('scissors');
+    });
 }
 
 main();
